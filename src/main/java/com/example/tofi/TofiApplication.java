8d8875e09873d95672ab7a63188005a4b4275ea8
liolegal <liolegal>
2023-11-06 17:40:10 +0300

    init

---
+++ src/main/java/com/example/tofi/TofiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TofiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tofi/TofiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
