4a786be8c88d975172b47c564790eff9bfc62193
liolegal <liolegal>
2023-12-02 19:47:15 +0300

    refactor dro to snake_case

--- src/main/java/com/example/tofi/common/persistance/domain/creditservice/Credit.java
+++ src/main/java/com/example/tofi/common/persistance/domain/creditservice/Credit.java
@@ -1,6 +1,7 @@
 package com.example.tofi.common.persistance.domain.creditservice;
 
 import com.example.tofi.common.persistance.domain.userservice.AbstractPersistentObject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -24,36 +25,47 @@ public class Credit extends AbstractPersistentObject {
     @Column(name = "date")
     LocalDateTime date;
 
+    @JsonProperty("user_id")
     @Column(name = "user_id")
     Long userId;
 
+    @JsonProperty("account_id")
     @Column(name = "account_id")
     Long accountId;
 
+    @JsonProperty("term")
     @Column(name = "term")
     CreditTerm term;
 
+    @JsonProperty("amount_given")
     @Column(name = "amount_given")
     Double amountGiven;
 
+    @JsonProperty("debt")
     @Column(name = "debt")
     Double debt;
 
+    @JsonProperty("next_pay_date")
     @Column(name = "next_pay_date")
     LocalDateTime nextPayDate;
 
+    @JsonProperty("per_month_pay_sum")
     @Column(name = "per_month_pay_sum")
     Double perMonthPaySum;
 
+    @JsonProperty("penya")
     @Column(name = "penya")
     Double penya;
 
+    @JsonProperty("status")
     @Column(name = "status")
     CreditStatus status;
 
+    @JsonProperty("payment_type")
     @Column(name = "payment_type")
     PaymentType paymentType;
 
+    @JsonProperty("is_notification_enabled")
     @Column(name = "is_notification_enabled")
     Boolean isNotificationEnabled;
 }
--- src/main/java/com/example/tofi/common/persistance/domain/creditservice/dto/CreditPaymentInfoDto.java
+++ src/main/java/com/example/tofi/common/persistance/domain/creditservice/dto/CreditPaymentInfoDto.java
@@ -1,5 +1,6 @@
 package com.example.tofi.common.persistance.domain.creditservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +11,19 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreditPaymentInfoDto {
 
+    @JsonProperty("credit_name")
     String creditName;
 
+    @JsonProperty("credit_id")
     Long creditId;
 
+    @JsonProperty("sum")
     Double sum;
 
+    @JsonProperty("penya")
     Double penya;
 
+    @JsonProperty("debt_after_payment")
     Double debtAfterPayment;
 
 }
